8341772548ede3d4eb5da7861bb8bb065fa89646
Rolando Ramirez <rolando.ramirezx@gmail.com>
2017-12-12 18:56:26 -0600

    Updated Main Activity

--- app/src/main/java/com/rolandoramirezx/settheoryandroid/MainActivity.kt
+++ app/src/main/java/com/rolandoramirezx/settheoryandroid/MainActivity.kt
@@ -2,11 +2,16 @@ package com.rolandoramirezx.settheoryandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calculate : Button = findViewById(R.id.Calculate)
+        
+
     }
 }
