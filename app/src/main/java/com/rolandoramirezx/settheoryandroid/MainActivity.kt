c565536ef1048f1133b46a88a4aec77bd3ee964f
Rolando Ramirez <rolando.ramirezx@gmail.com>
2017-12-13 18:26:01 -0600

    Updated Main Activity and Gradle

--- app/src/main/java/com/rolandoramirezx/settheoryandroid/MainActivity.kt
+++ app/src/main/java/com/rolandoramirezx/settheoryandroid/MainActivity.kt
@@ -3,9 +3,13 @@ package com.rolandoramirezx.settheoryandroid
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
+import org.springframework.web.client.RestTemplate
+import android.os.AsyncTask
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +27,24 @@ class MainActivity : AppCompatActivity() {
             val lines = input.text.toString().split("\n")
             //Then print out each line using Log.v
             lines.forEach { it-> Log.v("Input: ", it)}
+            HttpRequestTask().execute()
+
         }
     }
 }
+
+private class HttpRequestTask : AsyncTask<Void, Void, MessageEntity>() {
+    override fun doInBackground(vararg params: Void): MessageEntity? {
+    val url = "https://set-theory-web.herokuapp.com/api/settheory"
+        val restTemplate = RestTemplate()
+        restTemplate.messageConverters.add(MappingJackson2HttpMessageConverter())
+        return restTemplate.getForObject(url, MessageEntity::class.java)
+    }
+
+    override fun onPostExecute(messageEntity: MessageEntity) {
+    Log.v("output", messageEntity.toString())
+    }
+
+}
+
+data class MessageEntity(var message: String = "") //val = read only
