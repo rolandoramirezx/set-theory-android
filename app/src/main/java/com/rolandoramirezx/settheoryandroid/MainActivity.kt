b44c5a44e7af5846dd9cbb998e0a37f9411e65bd
Rolando Ramirez <rolando.ramirezx@gmail.com>
2017-12-18 19:25:11 -0600

    updated MainActivity.kt

--- app/src/main/java/com/rolandoramirezx/settheoryandroid/MainActivity.kt
+++ app/src/main/java/com/rolandoramirezx/settheoryandroid/MainActivity.kt
@@ -8,11 +8,15 @@ import android.widget.EditText
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
 import android.os.AsyncTask
+import android.widget.Toast
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 import java.net.URI
+import android.content.Intent
+import com.google.gson.Gson
+import java.io.Serializable
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,38 +30,62 @@ class MainActivity : AppCompatActivity() {
         //button function
         val calculate : Button = findViewById(R.id.Calculate)
         calculate.setOnClickListener {
-            //Log.v("Hello world", input.text.toString())
-            //TODO-Split the string by lines
             val lines = input.text.toString().split("\n")
             //Then print out each line using Log.v
             lines.forEach { it-> Log.v("Input: ", it)}
+            Toast.makeText(applicationContext,
+                    "Please wait while we process your request.", Toast.LENGTH_LONG).show();
             HttpRequestTask().execute(*Array(lines.size, { it -> lines[it]}))
 
         }
     }
+
+    inner class HttpRequestTask : AsyncTask<String, Void, Array<SetResult>>() {
+        override fun doInBackground(vararg params: String): Array<SetResult>? {
+            val url = "https://set-theory-web.herokuapp.com/api/settheory"
+            val restTemplate = RestTemplate()
+            restTemplate.messageConverters.add(MappingJackson2HttpMessageConverter())
+
+            val map = LinkedMultiValueMap<String, String>()
+            map.add("HeaderName", "value");
+            map.add("Content-Type", "application/json")
+            val entity = HttpEntity<Array<String>>(Array(params.size, {it -> params[it]}), map)
+
+            return restTemplate.exchange(URI(url), HttpMethod.POST, entity, Array<SetResult>::class.java).body
+        }
+
+        override fun onPostExecute(setResult: Array<SetResult>) {
+            val intent = Intent(this@MainActivity, ResultsActivity::class.java)
+
+            val json = Gson().toJson(setResult)
+            intent.putExtra("json", json)
+            this@MainActivity.startActivity(intent) //works because innerclasses can reach outterclasses
+        }
+
+    }
 }
 
-private class HttpRequestTask : AsyncTask<String, Void, Array<SetResult>>() {
-    override fun doInBackground(vararg params: String): Array<SetResult>? {
-        val url = "https://set-theory-web.herokuapp.com/api/settheory"
-        val restTemplate = RestTemplate()
-        restTemplate.messageConverters.add(MappingJackson2HttpMessageConverter())
+class ResultsActivity : AppCompatActivity() {
 
-        val map = LinkedMultiValueMap<String, String>()
-        map.add("HeaderName", "value");
-        map.add("Content-Type", "application/json")
-        val entity = HttpEntity<Array<String>>(Array(params.size, {it -> params[it]}), map)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultsactivity)
+        val intent = intent
 
-        return restTemplate.exchange(URI(url), HttpMethod.POST, entity, Array<SetResult>::class.java).body
+        val results = Gson().fromJson(intent.getStringExtra("json"), Array<SetResult>::class.java)
+        results.forEach { it-> Log.v(ResultsActivity::class.java.name, it.toString())}
     }
-
-    override fun onPostExecute(setResult: Array<SetResult>) {
-        Log.v("output", setResult.toString())
-    }
-
 }
+
+
+
+
+
+
+
+
 
 data class MessageEntity(var message: String = "") //val = read only
 
-data class SetResult(var set : String = "", var outcome : String = "")
+data class SetResult(var set : String = "", var outcome : String = ""): Serializable
 
